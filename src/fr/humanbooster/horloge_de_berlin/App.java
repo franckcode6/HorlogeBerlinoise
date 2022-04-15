a5f00d0ac033a10efb07f019695f4b33f0204a7b
Franckcode6 <franckcode6@gmail.com>
2022-04-15 15:20:52 +0200

    Ajout des commentaires

--- src/fr/humanbooster/horloge_de_berlin/App.java
+++ src/fr/humanbooster/horloge_de_berlin/App.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		// Parti declarative
+		// Partie declarative
 		int heures = 0;
 		int minutes = 0;
 		int moduloHeures = 0;
@@ -17,19 +17,26 @@ public class App {
 		Scanner scanner = new Scanner(System.in);
 
 		// Partie traitement
+		//On stock la saisie des heures
 		System.out.print("Entrez les heures (0-23) :");
 		heures = scanner.nextInt();
 
+		//On stock la saisie des minutes
 		System.out.print("Entrez les heures (0-59) :");
 		minutes = scanner.nextInt();
 		scanner.close();
-
+		
+		//Calcul des petits O
 		moduloHeures = heures % 5;
+		//Calcul des gros O
 		heures = (heures - moduloHeures) / 5;
 
+		//Calcul des petits X
 		moduloMinutes = minutes % 5;
+		//Calcul des gros X
 		minutes = (minutes - moduloMinutes) / 5;
 		
+		//Affichage des résultats
 		for (int i = 0; i < heures; i++) {
 			grandO += "O";
 		}
@@ -46,9 +53,13 @@ public class App {
 			petitX += "x";
 		}
 		
+		//Affichage des heures par 5
 		System.out.println(grandO);
+		//Affichage du restant des heures
 		System.out.println(petitO);
+		//Affichage des minutes par 5
 		System.out.println(grandX);
+		//Affichage du restant des minutes
 		System.out.println(petitX);
 	}
 }
